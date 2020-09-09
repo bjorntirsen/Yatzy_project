c224ff3cd7473f229dcbcce794422f6d98d18703
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2020-09-09 13:05:11 +0200

    javascript

---
+++ yatzy.js
@@ -0,0 +1,19 @@
+Document.addEventListener ("DOMContentLoaded", function(event) {
+    let player1one = document.getElementById("player1one").value;
+    let player1two = document.getElementById("player1two").value;
+    let player1three = document.getElementById("player1three").value;
+    let player1four = document.getElementById("player1four").value; 
+    let player1five = document.getElementById("player1five").value;
+    let player1six = document.getElementById("player1six").value;
+    
+    let player1sum = player1one + player1two + player1three + player1four + player1five + player1six;
+    
+    let player1bonus;
+
+    let total;
+    
+    
+    calc_btn.addEventListener("click", function(event) {
+        //beräkna-knapp
+    });
+});
