f710efa40d4c5241cb4496c5815f4269745d2443
Björn Tirsén <bjorntirsen@gmail.com>
2020-09-10 14:24:14 +0200

    Minor changes to js

--- js/yatzy.js
+++ js/yatzy.js
@@ -1,18 +1,17 @@
 document.addEventListener ("DOMContentLoaded", function(event) {
+    /*Skapar varabler från olika element i DOM:en*/
     let btnCalc = document.getElementById("calc-btn1");
     let sumField = document.getElementById("player1sum");
     let bonusField = document.getElementById("player1bonus");
+    /*Variabler från spelare etts fält*/
+    let p1onesField = document.getElementById("player1ones");
+    let p1twoesField = document.getElementById("player1twos");
+    let p1threesField = document.getElementById("player1threes");
+    let p1foursField = document.getElementById("player1fours"); 
+    let p1fivesField = document.getElementById("player1fives");
+    let p1sixesField = document.getElementById("player1sixes");
     
-    let player1one = document.getElementById("player1ones");
-    let player1two = document.getElementById("player1twos");
-    let player1three = document.getElementById("player1threes");
-    let player1four = document.getElementById("player1fours"); 
-    let player1five = document.getElementById("player1fives");
-    let player1six = document.getElementById("player1sixes");
-    
-    /*Vi får senare redigera denna kod för att möjliggöra funktionalietet för 
-    flera spelare: dvs. ändra sumField till p1sumField och bonusField till 
-    p1bonusField etc.
+    /*
     Kanske även ändra player1one till p1onesField etc. för att få mer enhetlig
      benämning på variablerna.
     */
@@ -21,36 +20,36 @@ document.addEventListener ("DOMContentLoaded", function(event) {
         let sum = 0;
         let tmp = 0;
 
-        tmp = player1one.value;
+        tmp = p1onesField.value;
         if (typeof(Number(tmp)) === "number") {
             sum += Number(tmp);
         }
 
         
-        tmp = player1two.value;
+        tmp = p1twoesField.value;
         if (typeof(Number(tmp)) === "number") {
             sum += Number(tmp);
         }
 
         
-        tmp = player1three.value;
+        tmp = p1threesField.value;
         if (typeof(Number(tmp)) === "number") {
             sum += Number(tmp);
         }
 
         
-        tmp = player1four.value;
+        tmp = p1foursField.value;
         if (typeof(Number(tmp)) === "number") {
             sum += Number(tmp);
         }
 
         
-        tmp = player1five.value;
+        tmp = p1fivesField.value;
         if (typeof(Number(tmp)) === "number") {
             sum += Number(tmp);
         }
         
-        tmp = player1six.value;
+        tmp = p1sixesField.value;
         if (typeof(Number(tmp)) === "number") {
             sum += Number(tmp);
         }
