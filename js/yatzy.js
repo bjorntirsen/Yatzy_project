c53b2d4924b0c250f29d6bbf2ace7894e501d99b
Björn Tirsén <bjorntirsen@gmail.com>
2020-09-21 08:16:10 +0200

    Merged css and js of main plus some styling

--- js/yatzy.js
+++ js/yatzy.js
@@ -188,4 +188,31 @@ document.addEventListener("DOMContentLoaded", function(event) {
             silver(td_p1y);
         }
     })
+    //Chatt script
+    let list = document.getElementById("ul_list");
+    let btnSend = document.getElementById("btnSend");
+
+    btnSend.addEventListener("click", function(e) {       
+        let divRow = document.createElement("divChat");
+        list.appendChild(divRow);
+        
+        let listRow = document.createElement("liChat");
+        divRow.appendChild(listRow);
+        
+        let spanRow = document.createElement("spanChat")
+        divRow.appendChild(spanRow);
+        spanRow.innerHTML = "TA BORT";
+        
+        let input = document.getElementById("inputText");
+        listRow.innerHTML = input.value;
+
+        input.value = "";
+        input.focus();
+    });
+    
+    list.addEventListener("click", function(event) {
+        if (event.target.tagName == "SPANCHAT") {
+    	    event.target.parentNode.remove();
+        }
+    })
 });
